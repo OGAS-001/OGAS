346517889bf8bca07dc2a910165443b779fdf2b5
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2022-09-13 17:35:33 +0800

    Update main.rs

--- Object_No.001/src/main.rs
+++ Object_No.001/src/main.rs
@@ -7,7 +7,7 @@ fn main(){
     2022.09.13
     测试成功,可以正常运行
     */
-    let url = "mysql://root:Zhangzerui+1s@124.223.13.92:3306/ttserver";
+    let url = "mysql://root:password@ipaddr:port/database";
     let pool = Pool::new(url).unwrap(); // 获取连接池
     let mut conn = pool.get_conn().unwrap();// 获取链接
     conn.query_iter("SELECT * from userinfo").unwrap().for_each(|row|
