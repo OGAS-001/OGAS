40f28534ee9802900b2afae523a29d74be638a93
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2023-05-08 20:04:16 +0800

    Update main.rs

--- Object_No.001/src/main.rs
+++ Object_No.001/src/main.rs
@@ -1,22 +1,51 @@
-use mysql::*;
+//use std::io;
 use mysql::prelude::*;
+use mysql::*;
 
+
+#[derive(Debug)]
+struct User {
+    id: u32,
+    username: String,
+    info: String,
+    group: u32,
+}
+
+impl User {
+    fn create(id: u32, username: String, info: String, group: u32) -> User {
+        User {
+            id,
+            username,
+            info,
+            group,
+        }
+    }
+}
 
 fn main(){
+    let mut users: Vec<User> = Vec::new();
+    //let u1 = User::create(0, "123".to_string(), "123".to_string(), 20213);
+    //println!("{}", u1.id);
     /*
     2022.09.13
     测试成功,可以正常运行
-    Debian系列可能会需要安装:librust-curl-sys+openssl-sys-dev
+    Debian: librust-curl-sys+openssl-sys-dev
+    Arch: pkg-config openssl
     */
-    let url = "mysql://root:password@ipaddr:port/database";
-    let pool = Pool::new(url).unwrap(); // 获取连接池
-    let mut conn = pool.get_conn().unwrap();// 获取链接
-    conn.query_iter("SELECT * from userinfo").unwrap().for_each(|row|
-    {
-        let r: (u8,String,String,String) = from_row(row.unwrap());
-        println!("{}, {},{},{}", r.0, r.1, r.2, r.3);
-    });
 
-
-
+    let url: &str = "mysql://root:my-secret-pw@127.0.0.1:3306/test";
+    let pool: Pool = Pool::new(url).unwrap(); // 获取连接池
+    let mut conn = pool.get_conn().unwrap(); // 获取链接
+    conn.query_iter("SELECT * from test_table")
+        .unwrap()
+        .for_each(|row| {
+            let r: (u32, String, String, u32) = from_row(row.unwrap());
+            let u: User = User::create(r.0, r.1, r.2, r.3);
+            users.push(u);
+        });
+    for i in users {
+        //println!("{},{},{},{}", i.id, i.username, i.info, i.group);
+        println!("{:?}", i);
+    }
+    
 }
